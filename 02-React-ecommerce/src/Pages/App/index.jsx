83aada2ec1c0d43f000830ed2b26626cc0079edd
Yunuen AM <yunameza@outlook.com>
2023-06-08 16:04:37 -0600

    ✨Feat:  Views added with import

--- 02-React-ecommerce/src/Pages/App/index.jsx
+++ 02-React-ecommerce/src/Pages/App/index.jsx
@@ -1,10 +1,20 @@
 import '@/App.css'
 import Home from '../Home'
+import MyAccount from '../MyAccount'
+import MyOrder from '../MyOrder'
+import MyOrders from '../MyOrders'
+import NotFound from '../NotFound'
+import SignIn from '../SignIn'
 
 function App () {
   return (
     <div className='bg-pink'>
       <Home />
+      <MyAccount />
+      <MyOrder />
+      <MyOrders />
+      <NotFound />
+      <SignIn />
     </div>
   )
 }
