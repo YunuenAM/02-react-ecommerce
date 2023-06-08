08dbc4adb7c0e3bdf531324ea0ec2ed7c3d59653
Yunuen AM <yunameza@outlook.com>
2023-06-08 16:37:47 -0600

    ✨Feat:  AppRoutes()  added

--- 02-React-ecommerce/src/Pages/App/index.jsx
+++ 02-React-ecommerce/src/Pages/App/index.jsx
@@ -6,13 +6,17 @@ import MyOrder from '../MyOrder'
 import MyOrders from '../MyOrders'
 import NotFound from '../NotFound'
 import SignIn from '../SignIn'
+// Bootstrap CSS
 
 const AppRoutes = () => {
   const routes = useRoutes([
-    {
-      path: '/',
-      element: <Home />
-    }
+    { path: '/', element: <Home /> },
+    { path: '/my-account', element: <MyAccount /> },
+    { path: '/my-order', element: <MyOrder /> },
+    { path: '/my-orders', element: <MyOrders /> },
+    { path: '/signIn', element: <SignIn /> },
+    { path: '/*', element: <NotFound /> }
+
   ])
   return routes
 }
--- 02-React-ecommerce/src/Pages/Home/index.jsx
+++ 02-React-ecommerce/src/Pages/Home/index.jsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='bg-pink'>Home</div>
   )
 }
 export default Home
