06b60a7938f33d8f252ec4fcfdd205bc367e50b3
Yunuen AM <yunameza@outlook.com>
2023-06-13 16:22:33 -0600

    💄 Feat: span + added to card

--- 02-React-ecommerce/src/Components/Card/index.jsx
+++ 02-React-ecommerce/src/Components/Card/index.jsx
@@ -1,7 +1,14 @@
 const Card = () => {
   return (
     <div className='card' style={{ width: '18rem' }}>
-      <img src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='card-img-top' alt='' />
+      <div className='position-relative'>
+        <img src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='card-img-top' alt='' />
+        <div className='position-absolute top-0 end-0 p-2'>
+          <div className='bg-primary text-white rounded-3 p-2'>
+            <span className='fw-bold'>+</span>
+          </div>
+        </div>
+      </div>
       <div className='card-body'>
         <h5 className='card-title'>Headphones</h5>
         <p className='card-text'>$700</p>
