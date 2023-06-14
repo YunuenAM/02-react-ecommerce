22eaab95ba72f81f674933d6204c3d4b7d71e1aa
Yunuen AM <yunameza@outlook.com>
2023-06-14 15:38:49 -0600

    ✨Feat:  calling the  API

--- 02-React-ecommerce/src/Components/Card/index.jsx
+++ 02-React-ecommerce/src/Components/Card/index.jsx
@@ -1,9 +1,9 @@
-const Card = () => {
+const Card = (data) => {
   return (
     <div className='card mx-auto' style={{ width: '18rem' }}>
       <div className='position-relative'>
-        <span className='fw-bold position-absolute bg-light rounded-5 m-2 p-1'>Electronics</span>
-        <img src='https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' className='card-img-top' alt='' />
+        <span className='fw-bold position-absolute bg-light rounded-5 m-2 p-1'>{data.data.category.name}</span>
+        <img src={data.data.images[0]} className='card-img-top' alt={data.data.title} />
         <div className='position-absolute top-0 end-0 p-2'>
           <div className='bg-primary text-white rounded-3 p-2'>
             <span className='fw-bold p-2'>+</span>
@@ -11,8 +11,8 @@ const Card = () => {
         </div>
       </div>
       <div className='card-body'>
-        <h5 className='card-title'>Headphones</h5>
-        <p className='card-text'>$700</p>
+        <h5 className='card-title'>{data.data.title}</h5>
+        <p className='card-text'>${data.data.price}</p>
       </div>
     </div>
   )
--- 02-React-ecommerce/src/Pages/Home/index.jsx
+++ 02-React-ecommerce/src/Pages/Home/index.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from 'react'
 import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 
 const Home = () => {
+  const [items, setItems] = useState(null)
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
-      <Card />
+
       Home
+      <div className='container text-center'>
+        <div className='row gap-4 mx-md-auto '>
+          {
+        items?.map(item => (
+          <Card key={item.id} data={item} />
+        ))
+      }
+
+        </div>
+      </div>
 
     </Layout>
   )
