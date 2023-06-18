01e1302470628c90d10cbd59745253d94346f04e
Yunuen AM <yunameza@outlook.com>
2023-06-18 16:17:19 -0600

    ✨Feat:  cart with card  added

--- 02-React-ecommerce/src/Components/CheckoutSideMenu/index.jsx
+++ 02-React-ecommerce/src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 import './checkoutsidemenu.scss'
+import OrderCard from '../OrderCard'
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
@@ -16,6 +17,18 @@ const CheckoutSideMenu = () => {
           <path d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z' />
         </svg>
 
+      </div>
+      <div className='px-4'>
+        {
+        context.cartProducts.map(product => (
+          <OrderCard
+            key={product.id}
+            title={product.title}
+            imageUrl={product.image}
+            price={product.price}
+          />
+        ))
+      }
       </div>
 
     </aside>
