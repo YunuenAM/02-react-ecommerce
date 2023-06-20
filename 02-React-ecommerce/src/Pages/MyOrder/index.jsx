35192c775fb60510bfec9e7d06b6fa5f4661c530
Yunuen AM <yunameza@outlook.com>
2023-06-20 15:42:55 -0600

    ✨Feat:  my orders  updated

--- 02-React-ecommerce/src/Pages/MyOrder/index.jsx
+++ 02-React-ecommerce/src/Pages/MyOrder/index.jsx
@@ -1,9 +1,27 @@
 import Layout from '../../Components/Layout'
-
+import { useContext } from 'react'
+import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../Components/OrderCard'
 const MyOrder = () => {
+  const context = useContext(ShoppingCartContext)
+
   return (
     <Layout>
       MyOrder
+      <div className='d flex-column w-50 '>
+        {
+        context.order?.slice(-1)[0].products.map(product => (
+          <OrderCard
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            imageUrl={product.image}
+            price={product.price}
+
+          />
+        ))
+      }
+      </div>
     </Layout>
   )
 }
