ebdc8b37630d63bf2a9691411f75b2e20bc9dad8
Yunuen AM <yunameza@outlook.com>
2023-06-22 16:05:44 -0600

    ✨Feat:  userService added

--- 02-React-ecommerce/src/Pages/SignUp/index.jsx
+++ 02-React-ecommerce/src/Pages/SignUp/index.jsx
@@ -1,86 +1,110 @@
+import { useNavigate } from 'react-router-dom'
+import useForm from '@/hooks/useForm'
+import { registerUserService } from '@/services/userService'
 import logo from '@/img/logo.jpg'
 import '@/styles/form.css'
-import Layout from '../../Components/Layout'
 
-const SignUp = () => {
+const Signup = () => {
+  // useNavigate() es un hook de react-router-dom que nos permite navegar entre rutas
+  const navigate = useNavigate()
+
+  const sendData = async (data) => {
+    try {
+      const response = await registerUserService(data)
+      if (response.status === 201) {
+        navigate('/login')
+        // console.log('Usuario creado correctamente', response.data)
+      }
+    } catch (error) {
+      console.log('Something is wrong:', error.message)
+    }
+  }
+
+  const { input, handleSubmit, handleInputChange } = useForm(sendData, {
+    first_name: '',
+    last_name: '',
+    gender: '',
+    email: '',
+    password: ''
+  })
+
   return (
-    <Layout>
-      <main className='form-signin w-100 m-auto'>
-        <form>
-          <img className='mb-4  align-items-center ' src={logo} alt='' width='200' height='200' />
-          <h1 className='h3 mb-3 fw-normal '>Please sign up</h1>
+    <main className='form-signin w-100 m-auto'>
+      <form onSubmit={handleSubmit}>
+        <img className='mb-4' src={logo} alt='' width='200' height='200' />
+        <h1 className='h3 mb-3 fw-normal'>Please sign up</h1>
 
-          <div className='form-floating'>
-            <input
-              type='text'
-              className='form-control'
-              id='first_name'
-              placeholder='John'
-              value=''
-              onChange={() => {}}
-            />
-            <label htmlFor='first_name'>First Name</label>
-          </div>
+        <div className='form-floating'>
+          <input
+            type='text'
+            className='form-control'
+            id='first_name'
+            name='first_name'
+            value={input.first_name}
+            onChange={handleInputChange}
+            placeholder='John'
+          />
+          <label htmlFor='first_name'>First Name</label>
+        </div>
 
-          <div className='form-floating'>
-            <input
-              type='text'
-              className='form-control'
-              id='last_name'
-              placeholder='Doe'
-              value=''
-              onChange={() => {}}
-            />
-            <label htmlFor='last_name'>Password</label>
-          </div>
+        <div className='form-floating'>
+          <input
+            type='text'
+            className='form-control'
+            id='last_name'
+            name='last_name'
+            value={input.last_name}
+            onChange={handleInputChange}
+            placeholder='Doe'
+          />
+          <label htmlFor='last_name'>Last Name</label>
+        </div>
 
-          <div className='form-floating'>
-            <select
-              className='form-select'
-              id='gender'
-              name='gender'
-              value=''
-              onChange={() => {}}
-            >
-              <option value=''>Choose...</option>
-              <option value='M'>Male</option>
-              <option value='F'>Female</option>
-              <option value='NB'>Non-binary</option>
-            </select>
-            <label htmlFor='gender'>Gender</label>
-          </div>
+        <div className='form-floating'>
+          <select
+            className='form-select'
+            id='gender'
+            name='gender'
+            value={input.gender}
+            onChange={handleInputChange}
+          >
+            <option value=''>Choose...</option>
+            <option value='M'>Male</option>
+            <option value='F'>Female</option>
+          </select>
+          <label htmlFor='gender'>Gender</label>
+        </div>
 
-          <div className='form-floating'>
-            <input
-              type='email'
-              className='form-control'
-              id='email'
-              name='email'
-              value=''
-              onChange={() => {}}
-              placeholder='name@example.com'
-            />
-            <label htmlFor='email'>Email address</label>
-          </div>
+        <div className='form-floating'>
+          <input
+            type='email'
+            className='form-control'
+            id='email'
+            name='email'
+            value={input.email}
+            onChange={handleInputChange}
+            placeholder='name@example.com'
+          />
+          <label htmlFor='email'>Email address</label>
+        </div>
 
-          <div className='form-floating'>
-            <input
-              type='password'
-              className='form-control'
-              id='password'
-              name='password'
-              value=''
-              onChange={() => {}}
-              placeholder='Password'
-            />
-            <label htmlFor='password'>Password</label>
-          </div>
+        <div className='form-floating'>
+          <input
+            type='password'
+            className='form-control'
+            id='password'
+            name='password'
+            value={input.password}
+            onChange={handleInputChange}
+            placeholder='Password'
+          />
+          <label htmlFor='password'>Password</label>
+        </div>
 
-          <button className='btn btn-pink w-100 py-2' type='submit'>Sign up</button>
-          <p className='mt-5 mb-3 text-body-secondary'>© 2017–2023</p>
-        </form>
-      </main>
-    </Layout>
+        <button className='w-100 btn btn-lg btn-primary' type='submit'>Sign up</button>
+        <p className='mt-5 mb-3 text-muted'>© 2017–2023</p>
+      </form>
+    </main>
   )
 }
-export default SignUp
+export default Signup
